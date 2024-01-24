8845a258d891824c18953fa8265b290e722f4571
murray <murray.buchanan.2022@uni.strath.ac.uk>
2024-01-24 18:32:04 +0000

    Added an ArrayList of accounts with the people and balance

--- src/main/java/uk/co/asepstrath/bank/App.java
+++ src/main/java/uk/co/asepstrath/bank/App.java
@@ -8,6 +8,7 @@ import io.jooby.hikari.HikariModule;
 import org.slf4j.Logger;
 
 import javax.sql.DataSource;
+import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -57,12 +58,18 @@ public class App extends Jooby {
     public void onStart() {
         Logger log = getLog();
         log.info("Starting Up...");
-//        new ArrayList TO BE WORKED ON
+        ArrayList<Account> accounts = new ArrayList<>();
+        accounts.add(new Account("Rachel", BigDecimal.valueOf(50.00)));
+        accounts.add(new Account("Monica", BigDecimal.valueOf(100.00)));
+        accounts.add(new Account("Phoebe", BigDecimal.valueOf(76.00)));
+        accounts.add(new Account("Joey", BigDecimal.valueOf(23.90)));
+        accounts.add(new Account("Chandler", BigDecimal.valueOf(3.00)));
+        accounts.add(new Account("Ross", BigDecimal.valueOf(54.32)));
+
         // Fetch DB Source
         DataSource ds = require(DataSource.class);
         // Open Connection to DB
         try (Connection connection = ds.getConnection()) {
-            //
             Statement stmt = connection.createStatement();
             stmt.executeUpdate("CREATE TABLE `Example` (`Key` varchar(255),`Value` varchar(255))");
             stmt.executeUpdate("INSERT INTO Example " + "VALUES ('WelcomeMessage', 'Welcome to A Bank')");
