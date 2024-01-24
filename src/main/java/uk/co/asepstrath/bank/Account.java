dd59374eb5610c589a06d3533170c26f1793d398
maikerudev <michenbox@protonmail.com>
2024-01-24 15:56:22 +0000

    Fix deposit fn

--- src/main/java/uk/co/asepstrath/bank/Account.java
+++ src/main/java/uk/co/asepstrath/bank/Account.java
@@ -1,11 +1,11 @@
 package uk.co.asepstrath.bank;
 
 public class Account {
-  int balance = 0;
+  private int balance = 0;
 
   public void deposit(int amount) {
     // TODO: This should probably throw an error.
-    if (amount > 0)
+    if (amount < 0)
       return;
     balance += amount;
   }
