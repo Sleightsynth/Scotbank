f62c41c76f38cb471d932cd514be7e13f3e8faf3
maikerudev <michenbox@protonmail.com>
2024-01-24 16:02:54 +0000

    Add withdraw method

--- src/main/java/uk/co/asepstrath/bank/Account.java
+++ src/main/java/uk/co/asepstrath/bank/Account.java
@@ -3,8 +3,15 @@ package uk.co.asepstrath.bank;
 public class Account {
   private int balance = 0;
 
+  public void withdraw(int amount) {
+    // TODO: This should probably throw an error instead.
+    if (amount < 0)
+      return;
+    balance -= amount;
+  }
+
   public void deposit(int amount) {
-    // TODO: This should probably throw an error.
+    // TODO: This should probably throw an error instead.
     if (amount < 0)
       return;
     balance += amount;
