0bedbff2494d1c985371e1adfe5aa4cd6add5a04
murray <murray.buchanan.2022@uni.strath.ac.uk>
2024-01-24 18:31:34 +0000

    Added second constructor method for testing and an override toString method

--- src/main/java/uk/co/asepstrath/bank/Account.java
+++ src/main/java/uk/co/asepstrath/bank/Account.java
@@ -3,9 +3,16 @@ package uk.co.asepstrath.bank;
 import java.math.BigDecimal;
 
 public class Account {
+  private String name;
   private BigDecimal balance = BigDecimal.valueOf(0);
 
-  public Account(String name, double balance) {
+  public Account(String name, BigDecimal balance) {
+    this.name = name;
+    this.balance = balance;
+  }
+
+  public Account() {
+//    For JUnit Testing
   }
 
   public void withdraw(BigDecimal amount) {
@@ -26,4 +33,8 @@ public class Account {
     return balance;
   }
 
+  @Override
+  public String toString() {
+    return "Name: " + name + ", Balance: £" + balance;
+  }
 }
