5ecf34f1427bba6ae6a320141ae44bd9c45df1b0
maikeru-dev <maikeru-dev@github.com>
2024-03-13 17:53:21 +0000

    Move Token struct

--- src/main/java/uk/co/asepstrath/bank/util/Token.java
+++ src/main/java/uk/co/asepstrath/bank/util/Token.java
@@ -1,6 +1,6 @@
-package uk.co.asepstrath.bank;
+package uk.co.asepstrath.bank.util;
 
-public class Token{
+public class Token {
     public String access_token;
     public String Token_type;
     public int expires_in;
